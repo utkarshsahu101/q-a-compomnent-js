89281a7a7b30e786d008b71ac507fe5300d47d67
Utkarsh Sahu <utkarsh.sahu@alphafortress.com>
2020-07-03 21:04:33 +0530

    using selectors

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 //using selectors inside the element
-// traversing the dom
 
-const btns = document.querySelectorAll(".question-btn");
+const questions = document.querySelectorAll(".question");
 
-btns.forEach(function (btn) {
-  btn.addEventListener("click", function (e) {
-    const question = e.currentTarget.parentElement.parentElement;
+questions.forEach(function (question) {
+  // console.log(question);
+  const btn = question.querySelector(".question-btn");
+  // console.log(btn);
+  btn.addEventListener("click", function () {
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
     question.classList.toggle("show-text");
   });
 });
+
+// traversing the dom
+
+// const btns = document.querySelectorAll(".question-btn");
+
+// btns.forEach(function (btn) {
+//   btn.addEventListener("click", function (e) {
+//     const question = e.currentTarget.parentElement.parentElement;
+//     question.classList.toggle("show-text");
+//   });
+// });
